c4f24a20ee9bf59dd4fef4eeee466ae405289df2
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-04-09 11:35:15 -0300

    creacion componente inicio, header y footer, y carpeta de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css'
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import Inicio from './components/views/Inicio';
 
 const App = () => {
   return (
     <div>
-      Hola mundo
+      <Header></Header>
+      <Inicio></Inicio>
+      <Footer></Footer>
     </div>
   );
 };
