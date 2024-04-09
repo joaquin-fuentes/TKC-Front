40c081041358b475554492f7c2d95eb914513ad3
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-04-09 12:23:42 -0300

    header completo

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import {Navbar, Container, Nav} from "react-bootstrap"
+import logoRojo from "../../assets/imagenes/logoRojo.jpg"
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import facebook from "../../assets/imagenes/icons8-facebook.svg"
+import whatsapp from "../../assets/imagenes/icons8-whatsapp.svg"
+import instagram from "../../assets/imagenes/icons8-instagram.svg"
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <Navbar expand="lg" className="bg-dark navBar header">
+        <Container>
+            <Navbar.Brand href="#home" className='text-light fw-bold'> 
+            <img src={logoRojo} alt="Logo joaquin fuentes" className='logoMenu rounded-circle me-1' /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className='bg-secondary' />
+            <Navbar.Collapse id="basic-navbar-nav " >
+                <Nav className="">
+                    <Nav.Link className="text-light text-header" href="#">Inicio</Nav.Link>
+                    <Nav.Link className="text-light text-header" href="#">Servicios</Nav.Link>
+                    <Nav.Link className="text-light text-header" href="#contacto">Contacto</Nav.Link>
+                    <Nav.Link className="text-light text-header" href="#">Nosotros</Nav.Link>
+                </Nav>
+                <Nav className="ms-auto d-flex">
+                    <Nav.Link className="text-light me-1 text-header" href="#"><img src={facebook} className='icono-header' alt="facebook" /></Nav.Link>
+                    <Nav.Link className="text-light me-1 text-header" href="#"><img src={whatsapp} className='icono-header' alt="whatsapp" /></Nav.Link>
+                    <Nav.Link className="text-light me-1 text-header" href="#"><img src={instagram} className='icono-header' alt="instagram" /></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
     );
 };
 
