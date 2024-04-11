f1fbc2740f73886c9ee134af5caeb976e60d0c61
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-04-11 11:12:24 -0300

    corregi tamaño de logo del header y el de inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -7,7 +7,7 @@ const Inicio = () => {
     return (
         <Container fluid className="contenedorPrincipal d-flex justify-content-center align-items-center">
             <section className='d-flex justify-content-center align-items-center flex-column'>
-                <img src={logoTransparente} alt="Logo de TKC" className=''/>
+                <img src={logoTransparente} alt="Logo de TKC" className='logoTkcInicio'/>
                 <Button variant="danger">Servicios</Button>
             </section>
         </Container>
