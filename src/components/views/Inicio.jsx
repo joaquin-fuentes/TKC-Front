a4252ce23252fcd7713f96147a60674868f88eeb
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-04-12 16:14:02 -0300

    cambios en diseño de cards de servicios

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -8,7 +8,7 @@ const Inicio = () => {
         <Container fluid className="contenedorPrincipal d-flex justify-content-center align-items-center">
             <section className='d-flex justify-content-center align-items-center flex-column'>
                 <img src={logoTransparente} alt="Logo de TKC" className='logoTkcInicio'/>
-                <Button variant="danger">Servicios</Button>
+                <Button variant="danger" className='w-50'>Servicios</Button>
             </section>
         </Container>
     );
--- src/components/views/Servicios.jsx
+++ src/components/views/Servicios.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Container, Row, Col, NavLink } from "react-bootstrap"
-import excursiones from "../../assets/imagenes/escuelita.jpg"
+import { Container, Row, Col, Button, Card, NavLink } from "react-bootstrap"
+import excursiones from "../../assets/imagenes/alquilerkayak.jpg"
 import coaching from "../../assets/imagenes/coaching.jpg"
-import escuelita from "../../assets/imagenes/portada2.jpg"
-import alquiler from "../../assets/imagenes/portada.jpg"
+import escuelita from "../../assets/imagenes/escuelita.jpg"
+import trekking from "../../assets/imagenes/trekking.jpg"
+import portada2 from "../../assets/imagenes/portada2.jpg"
+
+
+
+
 
 
 const Servicios = () => {
@@ -11,31 +16,53 @@ const Servicios = () => {
         <div className='bg-danger'>
             <Container className=''>
                 <h3 className='text-light m-0 p-4 text-center text-decoration-underline'> Nuestros Servicios</h3>
-                <Row className='pb-4'>
-                    <Col md="6" lg="6">
-                        <NavLink href='/'  className='d-flex justify-content-center align-items-center  m-3'>
-                            <img src={excursiones} alt="foto excursiones" className='w-100 imagenServicio' />
-                            <h4 className='text-img'>Excursiones</h4>
-                        </NavLink>
+                <Row className='pb-4 d-flex justify-content-center'>
+                    <Col md="6" lg="4" className='d-flex justify-content-center'>
+                        <Card className="bg-dark text-white p-0 card-servicio text-center">
+                            <Card.Img src={excursiones} alt="Card image" className='card-img'/>
+                            <Card.ImgOverlay className='d-flex flex-column justify-content-center align-items-center'>
+                                <Card.Title className='text-card'>Excursiones</Card.Title>
+                                <NavLink className='w-50  btn-card'>Ver más</NavLink>
+                            </Card.ImgOverlay>
+                        </Card>
                     </Col>
-                    <Col md="6" lg="6">
-                        <NavLink href='/' className='d-flex justify-content-center align-items-center  m-3'>
-                            <img src={coaching} alt="foto coaching" className='w-100 imagenServicio' />
-                            <h4 className='text-img'>Coaching Empresarial</h4>
-                        </NavLink>
+                    <Col md="6" lg="4" className='d-flex justify-content-center'>
+                        <Card className="bg-dark text-white p-0 card-servicio text-center">
+                            <Card.Img src={escuelita} alt="Card image" className='w-100 h-100 card-img'/>
+                            <Card.ImgOverlay className='d-flex flex-column justify-content-center align-items-center'>
+                                <Card.Title className='text-card'>Escuela de kayak</Card.Title>
+                                <NavLink className='w-50  btn-card'>Ver más</NavLink>
+                            </Card.ImgOverlay>
+                        </Card>
                     </Col>
-                    <Col md="6" lg="6">
-                        <NavLink href='/' className='d-flex justify-content-center align-items-center  m-3'>
-                            <img src={escuelita} alt="foto escuelita" className='w-100 imagenServicio' />
-                            <h4 className='text-img'>Escuela de Kayak</h4>
-                        </NavLink>
+                    <Col md="6" lg="4" className='d-flex justify-content-center'>
+                        <Card className="bg-dark text-white p-0 card-servicio text-center">
+                            <Card.Img src={coaching} alt="Card image" className='w-100 h-100 card-img'/>
+                            <Card.ImgOverlay className='d-flex flex-column justify-content-center align-items-center'>
+                                <Card.Title className='text-card'>Coaching empresarial</Card.Title>
+                                <NavLink className='w-50  btn-card'>Ver más</NavLink>
+                            </Card.ImgOverlay>
+                        </Card>
                     </Col>
-                    <Col md="6" lg="6">
-                        <NavLink href='/' className='d-flex justify-content-center align-items-center  m-3'>
-                            <img src={alquiler} alt="foto alquiler" className='w-100 imagenServicio' />
-                            <h4 className='text-img'>Alquiler de quincho para eventos</h4>
-                        </NavLink>
+                    <Col md="6" lg="4" className='d-flex justify-content-center'>
+                        <Card className="bg-dark text-white p-0 card-servicio text-center">
+                            <Card.Img src={portada2} alt="Card image" className='w-100 h-100 card-img'/>
+                            <Card.ImgOverlay className='d-flex flex-column justify-content-center align-items-center'>
+                                <Card.Title className='text-card'>Cumpleaños para niños</Card.Title>
+                                <NavLink className='w-50  btn-card'>Ver más</NavLink>
+                            </Card.ImgOverlay>
+                        </Card>
                     </Col>
+                    <Col md="6" lg="4" className='d-flex justify-content-center'>
+                        <Card className="bg-dark text-white p-0 card-servicio text-center">
+                            <Card.Img src={portada2} alt="Card image" className='w-100 h-100 card-img'/>
+                            <Card.ImgOverlay className='d-flex flex-column justify-content-center align-items-center'>
+                                <Card.Title className='text-card'>Alquiler de quincho para eventos</Card.Title>
+                                <NavLink className='w-50  btn-card'>Ver más</NavLink>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+
                 </Row>
             </Container>
         </div>
