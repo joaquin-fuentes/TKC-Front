4b11f772581b0ad845b69b8bd8a7d59034f62e9e
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-04-13 20:33:02 -0300

    agregue seccion de contacto, solo diseño

---
+++ src/components/views/Contacto.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap"
+import { RiContactsFill } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+
+
+const Contacto = () => {
+    return (
+        <Container>
+            <div className='d-flex flex-column justify-content-center align-items-center pt-3 text-danger'>
+                <RiContactsFill className='iconoKayak ' />
+                <h3 className=' m-0 p-4 text-center '> Podes contactarnos por whatsapp o cualquiera de nuestras redes sociales</h3>
+                <Row>
+                    <Col md={4}>
+                        <article className='d-flex flex-column justify-content-center align-items-center p-3 text-center'>
+                            <FaWhatsapp className='iconoWp' />
+                            <p className='textRedSocialContacto'>3813097754</p>
+                        </article>
+                    </Col>
+                    <Col md={4}>
+                        <article className='d-flex flex-column justify-content-center align-items-center p-3 text-center'>
+                            <FaFacebook className='iconoFb' />
+                            <p className='textRedSocialContacto'>Tucuman kayak club</p>
+                        </article>
+                    </Col>
+                    <Col md={4}>
+                        <article className='d-flex flex-column justify-content-center align-items-center p-3 text-center'>
+                           <FaInstagram className='iconoFb' />
+                            <p className='textRedSocialContacto'>@TucumankayakClub</p>
+                        </article>
+                    </Col>
+                </Row>
+            </div>
+        </Container>
+    );
+};
+
+export default Contacto;
