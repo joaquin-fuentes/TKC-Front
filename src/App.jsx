938ed6e5747496d10cbd69584e104c00fbd13751
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-08 21:36:22 -0300

    agregue pantalla de biendenida durante 3segundos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from './components/views/Inicio';
-import PaginaSobreNosotros from "./components/views/PaginaSobreNosotros"
+import PaginaSobreNosotros from "./components/views/PaginaSobreNosotros";
 import Excursiones from './components/views/servicios/Excursiones';
 import Escuela from './components/views/servicios/Escuela';
 import Coaching from './components/views/servicios/Coaching';
@@ -15,47 +15,69 @@ import Footer from './components/common/Footer';
 import Login from './components/views/Administrador/Login';
 import RutasProtegidas from "./components/routes/RutasProtegidas";
 import RutasAdministrador from "./components/routes/RutasAdministrador";
+import Loading from "./components/views/Loading";
 
 const App = () => {
+  const usuarioDelSessionStorage = JSON.parse(sessionStorage.getItem("usuarioLogueado")) || {};
+  const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioDelSessionStorage);
 
-  const usuarioDelSessionStorage = JSON.parse(sessionStorage.getItem("usuarioLogueado")) || {}
-  const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioDelSessionStorage)
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let mainPageLoaded = false;
+
+    const mainPageLoadHandler = () => {
+      // Página principal cargada, ahora esperar otros 3 segundos
+      mainPageLoaded = true;
+      setTimeout(() => {
+        setIsLoading(false); // Desactivar la carga después del retraso
+      }, 3000); // Esperar 3 segundos
+    };
+
+    // Temporizador para el tiempo de carga inicial
+    const initialLoadTimer = setTimeout(mainPageLoadHandler, 6000); // Tiempo para cargar página inicial
+
+    // Escuchar el evento onload
+    window.onload = () => {
+      if (!mainPageLoaded) {
+        clearTimeout(initialLoadTimer);
+        mainPageLoadHandler();
+      }
+    };
+
+    return () => {
+      clearTimeout(initialLoadTimer); // Limpia el temporizador cuando el componente se desmonta
+      window.removeEventListener('load', mainPageLoadHandler);
+    };
+  }, []);
 
   return (
-    <BrowserRouter>
-      <div>
-        <Header usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Header>
-        <div className="main">
-          <Routes>
-            <Route exact path="/" element={<Inicio></Inicio>
-            }></Route>
-            <Route exact path="/sobrenosotros" element={<PaginaSobreNosotros></PaginaSobreNosotros>
-            }></Route>
-            <Route exact path="/excursiones" element={<Excursiones></Excursiones>
-            }></Route>
-            <Route exact path="/escuela" element={<Escuela></Escuela>
-            }></Route>
-            <Route exact path="/coaching" element={<Coaching></Coaching>
-            }></Route>
-            <Route exact path="/cumpleaños" element={<Cumpleanios></Cumpleanios>
-            }></Route>
-            <Route exact path="/alquiler" element={<Alquiler></Alquiler>
-            }></Route>
-            <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado}></Login>
-            }></Route>
-            <Route path="/administrador/*" element={
-              <RutasProtegidas>
-                <RutasAdministrador></RutasAdministrador>
-              </RutasProtegidas>
-            }></Route>
-            <Route path="*" element={<Error404></Error404>
-            }></Route>
-          </Routes>
-        </div>
-
-        <Footer></Footer>
-      </div>
-    </BrowserRouter>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <BrowserRouter>
+          <div>
+            <Header usuarioLogueado={usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado} />
+            <div className="main">
+              <Routes>
+                <Route exact path="/" element={<Inicio />} />
+                <Route exact path="/sobrenosotros" element={<PaginaSobreNosotros />} />
+                <Route exact path="/excursiones" element={<Excursiones />} />
+                <Route exact path="/escuela" element={<Escuela />} />
+                <Route exact path="/coaching" element={<Coaching />} />
+                <Route exact path="/cumpleaños" element={<Cumpleanios />} />
+                <Route exact path="/alquiler" element={<Alquiler />} />
+                <Route exact path="/login" element={<Login setUsuarioLogueado={setUsuarioLogueado} />} />
+                <Route path="/administrador/*" element={<RutasProtegidas><RutasAdministrador /></RutasProtegidas>} />
+                <Route path="*" element={<Error404 />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      )}
+    </>
   );
 };
 
---
+++ src/components/views/Loading.jsx
@@ -0,0 +1,24 @@
+// Loading.js
+import React from 'react';
+import cargando from "../../assets/imagenes/cargando-blanco.gif"
+import logoTransparente from "../../assets/imagenes/logoTransparente.png"
+import { Spinner } from 'react-bootstrap';
+
+
+const Loading = () => {
+  return (
+    <div className='fondoPaginaCarga'>
+      <img src={logoTransparente} alt="Logo TKC" className='logoPaginaCarga' />
+      {/* <img src={cargando} alt="Loading..." className='iconoPaginaCarga' /> */}
+      <h4 className='text-light'>¡BIENVENIDOS!</h4>
+      <div>
+        <Spinner className='mx-1' animation="grow" variant="light" />
+        <Spinner className='mx-1' animation="grow" variant="light" />
+        <Spinner className='mx-1' animation="grow" variant="light" />
+      </div>
+
+    </div>
+  );
+};
+
+export default Loading;
