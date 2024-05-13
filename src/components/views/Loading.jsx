f570386aa8bd5ccaa44f99fe9fbd801eb2f509de
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-05-13 09:02:39 -0300

    cambie tamaño de letras y colores en subitulos de excursiones

--- src/components/views/Loading.jsx
+++ src/components/views/Loading.jsx
@@ -10,12 +10,12 @@ const Loading = () => {
     <div className='fondoPaginaCarga'>
       <img src={logoTransparente} alt="Logo TKC" className='logoPaginaCarga' />
       {/* <img src={cargando} alt="Loading..." className='iconoPaginaCarga' /> */}
-      <h4 className='text-light fs-2 textPaginaCarga'>¡BIENVENIDOS!</h4>
+      {/* <h4 className='text-light fs-2 textPaginaCarga'>¡BIENVENIDOS!</h4>
       <div>
         <Spinner className='mx-1' animation="grow" variant="light" />
         <Spinner className='mx-1' animation="grow" variant="light" />
         <Spinner className='mx-1' animation="grow" variant="light" />
-      </div>
+      </div> */}
 
     </div>
   );
