4d8546ef92049bf78920f2d44cd5d986edb34a89
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-09-07 21:00:09 -0300

    agregue hook para smoothScrool y color de fondo de header

--- src/components/views/Portada.jsx
+++ src/components/views/Portada.jsx
@@ -1,33 +1,41 @@
 import React from "react";
-import logoTransparente from "../../assets/logos/logoTransparente.webp"; // Asegúrate de tener el logo aquí
+import logoTransparente from "../../assets/logos/logoTransparente.webp";
 import portada from "../../assets/imagenes/portada.webp";
+
 const Portada = () => {
   return (
-    <div
-      className="h-screen w-full bg-cover bg-bottom flex flex-col justify-center items-center "
+    <header
+      className="relative h-screen w-full bg-cover bg-center flex flex-col justify-center items-center"
       style={{
-        backgroundImage: `url(${portada})`, // Asegúrate de reemplazar portada con la ruta de tu imagen
+        backgroundImage: `url(${portada})`, // Ruta de la imagen
       }}
       id="inicio"
     >
-      {/* Oscurecer la imagen ligeramente para mejorar la visibilidad del contenido */}
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      {/* Oscurecer la imagen de fondo para mejorar la visibilidad del contenido */}
+      <div className="absolute inset-0 bg-black bg-opacity-50 h-full"></div>
 
       {/* Contenido de la portada */}
       <div className="relative z-10 text-center">
+        {/* Título principal oculto para SEO, pero no visible */}
+        <h1 className="sr-only">Bienvenidos a Tucumán Kayak Club</h1>
+
+        {/* Logo de la empresa */}
         <img
           src={logoTransparente}
-          alt="Logo de TKC"
+          alt="Logo de Tucumán Kayak Club"
           className="w-64 mx-auto mb-8 drop-shadow-xl"
         />
+
+        {/* Botón con descripción accesible */}
         <a
           href="/#servicios"
           className="px-6 py-3 bg-red-600 text-white rounded-full hover:bg-red-500 transition duration-300 font-bold text-lg"
+          aria-label="Ver nuestros servicios"
         >
           Nuestros Servicios
         </a>
       </div>
-    </div>
+    </header>
   );
 };
 
